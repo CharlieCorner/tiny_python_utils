b498afa5ed2c83cc3bd8ffaa793f55de37063d4c
Charlie Corner <--global>
2017-07-11 01:44:25 -0400

    Add the first version of the script

---
+++ src/vibrate_mouse.py
@@ -0,0 +1,41 @@
+import pyautogui
+import time
+from argparse import ArgumentParser
+
+
+def main():
+    args = _parse_args()
+
+    if args.timeout_time >= 1:
+        for i in range(0, args.timeout_time):
+            print("Second: %i" % i)
+            vibrate_cursor()
+    else:
+        while True:
+            vibrate_cursor()
+
+
+def vibrate_cursor():
+    x, y = pyautogui.position()
+    # print("X: %i, Y: %i" % (x, y))
+    pyautogui.moveTo(x + 35, y + 35)
+    time.sleep(0.10)
+    pyautogui.moveTo(x, y)
+
+
+def _parse_args():
+    """Parse args with argparse
+    :returns: args
+    """
+    parser = ArgumentParser(description="Make the mouse cursor vibrate.")
+
+    parser.add_argument('-t',
+                        default=-1,
+                        dest="timeout_time",
+                        type=int,
+                        help="The amount of time in seconds before exiting. Any value less than 1 equals to infinity. Defaults to: %default")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
